1173f607a1c4f77cc83e34503d181c10ba9afdb4
udogan <udogan@DESKTOP-LGQOV9B>
2019-06-20 11:35:22 +0300

    Oyun Tamamlandı Gibi geliyor

--- YilanOyunu/src/com/udogan/yilanoyunu/Ekran.java
+++ YilanOyunu/src/com/udogan/yilanoyunu/Ekran.java
@@ -1,19 +1,39 @@
 package com.udogan.yilanoyunu;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Ekran {
 
 	public int x;
 	public int y;
+	public int yeminYeri = 0;
+	int puan = 0;
 
 	public Ekran() {
 
 	}
 
 	public void doldur(int[] yilan) {
+		Random rand = new Random();
+
+		while (IntStream.of(yilan).anyMatch(i -> i == yeminYeri) || yeminYeri == 0) {
+			if (yilan[0] == yeminYeri) {
+				yeminYeri = rand.nextInt(this.x * this.y) + 1;
+				puan = puan + 10;
+			} else {
+				yeminYeri = rand.nextInt(this.x * this.y) + 1;
+			}
+		}
+
 		for (int i = 0; i < y; i++) {
 			for (int j = 1; j <= x; j++) {
 				if (i * x + j == yilan[0]) {
 					System.out.print(" O ");
+					continue;
+				}
+				if (i * x + j == yeminYeri) {
+					System.out.print(" * ");
 					continue;
 				}
 				boolean yilaninParcasi = false;
@@ -27,12 +47,13 @@ public class Ekran {
 				}
 				if (yilaninParcasi) {
 					System.out.print(" X ");
-				}
-				else {
+				} else {
 					System.out.print(" . ");
 				}
 			}
 			System.out.println();
 		}
+
+		System.out.println("\nPuan: " + this.puan);
 	}
 }
--- YilanOyunu/src/com/udogan/yilanoyunu/Yilan.java
+++ YilanOyunu/src/com/udogan/yilanoyunu/Yilan.java
@@ -1,5 +1,7 @@
 package com.udogan.yilanoyunu;
 
+import java.util.stream.IntStream;
+
 public class Yilan {
 
 	private Ekran ekran;
@@ -18,51 +20,83 @@ public class Yilan {
 	}
 
 	public void sag() {
-		if (yilanKonum[0] % ekran.x == 0 ) {
-			System.out.println("SA - Oyun Bitti!");
+		if (yilanKonum[0] % ekran.x == 0) {
+			System.out.println("Duvara tosladýn - Oyun Bitti!");
 			this.oyunBitti = true;
 			return;
 		}
 		yilaninHareketi(1);
-		yilanYerlestir();
+		if (!oyunBitti) {
+			yilanYerlestir();
+		}		
 	}
 
 	public void sol() {
-		if (yilanKonum[0] % ekran.x == 1 ) {
-			System.out.println("SO - Oyun Bitti!");
+		if (yilanKonum[0] % ekran.x == 1) {
+			System.out.println("Duvara tosladýn - Oyun Bitti!");
 			this.oyunBitti = true;
 			return;
-		}		
+		}
 		yilaninHareketi(-1);
-		yilanYerlestir();
+		if (!oyunBitti) {
+			yilanYerlestir();
+		}
 	}
 
 	public void yukari() {
 		if (yilanKonum[0] < ekran.x) {
-			System.out.println("Y - Oyun Bitti!");
+			System.out.println("Duvara tosladýn - Oyun Bitti!");
 			this.oyunBitti = true;
 			return;
 		}
 		yilaninHareketi(-ekran.x);
-		yilanYerlestir();
+		if (!oyunBitti) {
+			yilanYerlestir();
+		}
 	}
 
 	public void asagi() {
-		if (yilanKonum[0] > (ekran.y-1) * ekran.x) {
-			System.out.println("A - Oyun Bitti!");
+		if (yilanKonum[0] > (ekran.y - 1) * ekran.x) {
+			System.out.println("Duvara tosladýn - Oyun Bitti!");
 			this.oyunBitti = true;
 			return;
 		}
 		yilaninHareketi(ekran.x);
-		yilanYerlestir();
+		if (!oyunBitti) {
+			yilanYerlestir();
+		}
 	}
 
 	private void yilaninHareketi(int yilaninHareketi) {
-		if (yilanKonum[0] + yilaninHareketi == yilanKonum[1]) {
+		if (IntStream.of(yilanKonum).anyMatch(i -> i == yilanKonum[0] + yilaninHareketi)) {
+			System.out.println("Isýrýldýn - Oyun Bitti!");
+			this.oyunBitti = true;
 			return;
 		}
-		yilanKonum[2] = yilanKonum[1];
-		yilanKonum[1] = yilanKonum[0];
-		yilanKonum[0] = yilanKonum[0] + yilaninHareketi;
+		
+		if (yilanKonum[0] + yilaninHareketi == ekran.yeminYeri) {
+			if (yilanKonum.length + 1 == ekran.y * ekran.x) {
+				System.out.println("Tebrikler Oyunu Kazandýn\n\nPuan: " + (ekran.puan + 10));
+			}
+			
+			int[] tempYilanKonum = new int[yilanKonum.length + 1];
+			tempYilanKonum[0] = yilanKonum[0] + yilaninHareketi;
+			for (int i = 0; i < yilanKonum.length; i++) {
+				tempYilanKonum[i + 1] = yilanKonum[i];
+			}
+			yilanKonum = tempYilanKonum;
+		} else {
+			if (yilanKonum[0] + yilaninHareketi == yilanKonum[1]) {
+				return;
+			}
+
+			for (int i = yilanKonum.length - 1; i >= 0; i--) {
+				if (i == 0) {
+					yilanKonum[0] = yilanKonum[0] + yilaninHareketi;
+				} else {
+					yilanKonum[i] = yilanKonum[i-1];
+				}
+			}
+		}
 	}
 }
